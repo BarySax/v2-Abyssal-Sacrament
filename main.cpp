2361ea3a464510d47760fa8d64b228d681b061da
BarySax <144921224+BarySax@users.noreply.github.com>
2023-12-13 19:52:07 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,20 @@ struct player
     int hp = 100;
     int xp = 0;
     string name;
-    string type;
+    string origin;
 };
 
 int main() 
 {
     player myPlayer;
     myPlayer.name = createPlayer();
-    myPlayer.type = setPlayerType();
-    
-
-    cout << "Bonjour " << myPlayer.name << " le " << myPlayer.type << "\n";
+    myPlayer.origin = setPlayerOrigin();
+    if (myPlayer.origin == "Sans dessein") {
+        cout << "Salutation, " << myPlayer.name << ", dans ce monde froid, vous n'êtes rien\n";
+    } else if (myPlayer.origin == "Sans dessein") {
+        cout << "Salutation, " << myPlayer.name << ", vous êtes de toute évidence pure\n";
+    } else if (myPlayer.origin == "Pieux") {
+        cout << "Salutation, " << myPlayer.name << ", de toute évidence, votre vie n'a été que de servir votre saint patron\n";
+    }
     return 0;
 }
