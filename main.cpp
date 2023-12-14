450683a75689eef7bf15d9dbe344779798184451
steiyve <94018684+steiyve@users.noreply.github.com>
2023-12-14 09:21:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,12 +15,6 @@ int main()
     player myPlayer;
     myPlayer.name = createPlayer();
     myPlayer.origin = setPlayerOrigin();
-    if (myPlayer.origin == "Sans dessein") {
-        cout << "Salutation, " << myPlayer.name << ", dans ce monde froid, vous n'êtes rien\n";
-    } else if (myPlayer.origin == "Sans dessein") {
-        cout << "Salutation, " << myPlayer.name << ", vous êtes de toute évidence pure\n";
-    } else if (myPlayer.origin == "Pieux") {
-        cout << "Salutation, " << myPlayer.name << ", de toute évidence, votre vie n'a été que de servir votre saint patron\n";
-    }
+    
     return 0;
 }
