bc357fd3fa3df71b0237e6fbab6a88a1f21f500b
nicolas <priggen@educ.college-montreal.qc.ca>
2024-01-09 14:29:21 -0500

    les deux premiere tache

---
+++ src/tache.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unistd.h>
+#include "fight.h"
+
+using namespace std;
+
+bool livraison(string allerA)
+{
+    cout << "tu te mes en chemin pour aller a " << allerA << endl;
+    sleep(1);
+    cout << "tu rencontre un chien gentill, et tu le carresse" << endl;
+    cout << "tu continue ton chemin" << endl;
+    sleep(1);
+    cout << "tu arrive a " << allerA << endl;
+    return true;
+}
+
+bool trouverObjet(string objet, int playerHp, int attaque)
+{
+    cout << "tu te mes en chemin pour trouver " << objet << endl;
+    sleep(1);
+    cout << "tu voit une creature et tu la combat" << endl;
+    
+    if (fight(playerHp, attaque) == 0)
+    {
+        return false;
+    }
+    else
+    {
+        cout << "tu a gagner le combat" << endl;
+        sleep(1);
+        cout << "tu continue ton chemin" << endl;
+        sleep(1);
+        cout << "tu trouve " << objet << endl;
+        return true;
+    }
+
+}
